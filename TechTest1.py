a5d8a4314fa13cf91409db5ad40d6cafa2dbfe5a
Ronald Rodriguez <ronrf095@hotmail.com>
2023-03-02 14:42:40 -0500

    Agregando nuevas lineas

--- TechTest1.py
+++ TechTest1.py
@@ -1,7 +1,22 @@
-# En la variable 'Text' dispones de una cadena de texto
+# En la variable 'text' dispones de una cadena de texto
 # debes contar las palabras y devolver cuantas vececs se repiten cada una de ellas
 # Ejemplo --> 'nada' aparece 2 veces
 
 
-Text = "Creo que a veces es la gente de la que nadie espera nada, la que hace cosas que nadie puede imaginar."
+text = "Creo que a veces es la gente de la que nadie espera nada, la que hace cosas que nadie puede imaginar."
+
+
+#def QuitCharac (Text):
+
+
+def ToLowerCAse (text):
+    lowrCase = text.lower()
+    return lowrCase
+
+
+lowcas = ToLowerCAse(text)
+
+print(lowcas)
+
+
 
