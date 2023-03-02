3444ce9319cbaf2c0db298768520887594555c6f
Ronald Rodriguez <ronrf095@hotmail.com>
2023-03-02 18:25:34 -0500

    Actualizando

--- TechTest1.py
+++ TechTest1.py
@@ -6,17 +6,31 @@
 text = "Creo que a veces es la gente de la que nadie espera nada, la que hace cosas que nadie puede imaginar."
 
 
-#def QuitCharac (Text):
 
 
+#with this you conver string to lowecase
 def ToLowerCAse (text):
-    lowrCase = text.upper()
+    lowrCase = text.lower()
     return lowrCase
 
+#with this you clean the special characters in the string and convert to list
+def CleanCharac (Text):
+    ClenChart = Text.replace(',','').replace('.','')
+    Separate = ClenChart.split(' ')
+    return Separate
 
-lowcas = ToLowerCAse(text)
+#wtih this you count any word in the list and return a dictionary with the count number
+def CountWords (List):
+    dictionary = {}
+    for word in List:
+        if word in dictionary:
+            dictionary[word] += 1
+        else:
+            dictionary[word] = 1
+    return dictionary
 
-print(lowcas)
+
+a = ['paula','paula','paula','paula','camila','camila','camila','camila','camila','camila']
 
 
-
+print(CountWords(CleanCharac(ToLowerCAse(text))))
