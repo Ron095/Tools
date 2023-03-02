5a01b620de224690c420392f7c48c354554918ff
Ronald Rodriguez <ronrf095@hotmail.com>
2023-03-02 14:15:06 -0500

    Nuevo Ejercicio de practica

---
+++ TechTest1.py
@@ -0,0 +1,7 @@
+# En la variable 'Text' dispones de una cadena de texto
+# debes contar las palabras y devolver cuantas vececs se repiten cada una de ellas
+# Ejemplo --> 'nada' aparece 2 veces
+
+
+Text = "Creo que a veces es la gente de la que nadie espera nada, la que hace cosas que nadie puede imaginar."
+
